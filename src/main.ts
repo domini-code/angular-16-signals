73cc8c456601f5419a61bb7348a74b19e9e2c2f9
bezael <bezael@gmail.com>
2023-04-19 22:18:19 +0200

    functional route

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,30 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-
-import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
-import { AppRoutingModule } from './app/app-routing.module';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { Routes, provideRouter } from '@angular/router';
+import { AuthGuard } from './app/shared/guards/auth.guard';
 
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('@pages/home/home.component').then((c) => c.HomeComponent),
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('@pages/login/login.component').then((c) => c.LoginComponent),
+  },
+  {
+    path: 'private',
+    loadComponent: () =>
+      import('@pages/private/private.component').then(
+        (c) => c.PrivateComponent
+      ),
+    canActivate: [AuthGuard],
+  },
+];
 
 bootstrapApplication(AppComponent, {
-    providers: [importProvidersFrom(BrowserModule, AppRoutingModule)]
-})
-  .catch(err => console.error(err));
+  providers: [provideRouter(routes)],
+}).catch((err) => console.error(err));
