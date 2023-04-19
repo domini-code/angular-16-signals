19e337bd616e92307f64421d2e068a3291ad7b94
bezael <bezael@gmail.com>
2023-04-19 22:42:35 +0200

    initial

--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { CanActivate, Router, UrlTree } from '@angular/router';
-import { Observable, map, take } from 'rxjs';
+import { Observable, take, tap } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
@@ -11,21 +11,13 @@ export class AuthGuard implements CanActivate {
   private readonly router = inject(Router);
 
   canActivate():
-    | Observable<boolean | UrlTree>
-    | Promise<boolean | UrlTree>
+    | Observable<boolean>
     | boolean
     | UrlTree {
-    debugger;
     return this.authService.isAuthenticated$().pipe(
       take(1),
-      map((isLoggedIn) => {
-        debugger;
-        if (isLoggedIn) {
-          return true;
-        } else {
-          return this.router.createUrlTree(['/login']);
-        }
-      })
+      tap((isLoggedIn) => !isLoggedIn ? this.router.navigate(['/login']) : true)
     );
   }
 }
+
